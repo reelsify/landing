dfe671961b3d98c340b5abcc6785f02e7718faad
Aravind Karnam <aravindkarnam@Aravinds-Mac-Studio.local>
2024-05-28 10:12:31 +0530

    fixed rounded corners on images

--- src/components/feature.js
+++ src/components/feature.js
@@ -23,7 +23,7 @@ export default function Feature(props) {
               <p className="mt-6 text-lg leading-8 text-gray-600">
                 {props.text}
               </p>
-              {props.links && props.links.length > 0 && (
+              {/* {props.links && props.links.length > 0 && (
                 <div className="mt-6">
                   {" "}
                   <a
@@ -33,7 +33,7 @@ export default function Feature(props) {
                     {props.links[0].text} <GrFormNextLink className="inline" />
                   </a>
                 </div>
-              )}
+              )} */}
               {/* <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
               {features.map((feature) => (
                 <div key={feature.name} className="relative pl-9">
@@ -48,13 +48,13 @@ export default function Feature(props) {
             </div>
           </div>
           <div className="-mt-8 md:mt-0 px-4 md:px-0 py-4 lg:py-8 rounded-b-xl lg:rounded-r-xl bg-gradient-to-b lg:bg-gradient-to-r
-        from-slate-100 via-blue-100 to-blue-800 via-15% lg:via-30%">
+        from-slate-300 lg:from-slate-100 via-blue-300 lg:via-blue-100 to-blue-800 lg:to-blue-800 via-15% lg:via-30%">
             {props.image && (
               <GatsbyImage
                 image={getImage(props.image.gatsbyImageData)}
                 alt={props.image.alt}
                 className={
-                  "max-w-lg rounded-xl  sm:w-[57rem] md:-ml-4 lg:-ml-0 "
+                  "max-w-lg sm:w-[57rem] md:-ml-4 lg:-ml-0 "
                 }
                 width={500}
                 height={500}
@@ -64,27 +64,6 @@ export default function Feature(props) {
         </div>
       </div>
     </div>
-    // <Section padding={4} >
-    //   <Container>
-    //     <Flex gap={4} variant="responsive">
-    //       <Box width="half" order={props.flip ? 1 : null}>
-    //         {props.image && (
-    //           <GatsbyImage
-    //             alt={props.image.alt}
-    //             image={getImage(props.image.gatsbyImageData)}
-    //           />
-    //         )}
-    //       </Box>
-    //       <Box width="half">
-    //         <Subhead>
-    //           {props.heading}
-    //         </Subhead>
-    //         <Text variant="lead">{props.text}</Text>
-    //         <ButtonList links={props.links} />
-    //       </Box>
-    //     </Flex>
-    //   </Container>
-    // </Section>
   )
 }
 
